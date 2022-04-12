a601aef1bf3a0e06b89e61792379a8477149fce3
Rick Hull <rwh@localhost>
2022-04-12 19:20:39 -0400

    add test for the basic tire fitment model

--- test/fitment.rb
+++ test/fitment.rb
@@ -11,4 +11,17 @@ describe Fitment do
     expect(Fitment.mm 1).must_equal 25.4
     expect(Fitment.mm 12).must_equal 25.4 * 12
   end
+
+  it "has a simple linear tire to wheel fitment model" do
+    # from https://www.tiresandco.ca/tire-equivalence-advice.html
+    src_data = {
+      6.0 => [175, 185, 195, 205],
+      8.5 => [225, 235, 245, 255],
+      12.5 => [305, 315, 325, 335],
+    }
+
+    src_data.each { |rim_width, tire_widths|
+      expect(Fitment.tire_widths(rim_width)).must_equal tire_widths
+    }
+  end
 end
