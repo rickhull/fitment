d4193e8c6f6cac99bec32211b7e2a85106b02dac
Rick Hull <rick.hull@gmail.com>
2022-05-07 17:51:48 -0400

    use Rational; slight reorg

--- lib/fitment/wheel.rb
+++ lib/fitment/wheel.rb
@@ -14,18 +14,18 @@ module Fitment
     attr_accessor :bolt_pattern
 
     def initialize(diameter_in, width_in, et = 0, bolt_pattern: "")
-      @diameter = diameter_in
-      @width = width_in
-      @et = et
-      @bolt_pattern = bolt_pattern
-    end
-
-    def to_s
-      "#{@diameter}x#{@width} ET#{et}  #{@bolt_pattern}".strip
+      @diameter = Rational(diameter_in)
+      @width = Rational(width_in)
+      @et = et.to_i
+      @bolt_pattern = bolt_pattern.to_s.strip
     end
 
     def offset
       self.class.offset(@et, @width)
+    end
+
+    def to_s
+      "%gx%g ET%i %s" % [@diameter, @width, @et, @bolt_pattern]
     end
   end
 
@@ -33,18 +33,18 @@ module Fitment
     attr_reader :offset
 
     def initialize(diameter_in, width_in, offset_in, bolt_pattern: "")
-      @diameter = diameter_in
-      @width = width_in
-      @offset = offset_in
-      @bolt_pattern = bolt_pattern
-    end
-
-    def to_s
-      "#{@diameter}x#{@width} #{@offset}\" offset  #{@bolt_pattern}".strip
+      @diameter = Rational(diameter_in)
+      @width = Rational(width_in)
+      @offset = Rational(offset_in)
+      @bolt_pattern = bolt_pattern.to_s.strip
     end
 
     def et
       self.class.et(@offset, @width)
+    end
+
+    def to_s
+      "%gx%g offset %g %s" % [@diameter, @width, @offset, @bolt_pattern]
     end
   end
 end
